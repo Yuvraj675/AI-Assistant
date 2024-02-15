c88d5affc70c5a503c6d1fd3500ab90152b86a86
Yuvraj-G <yuviyg675@gmail.com>
2024-02-15 21:36:39 +0530

    Initial Commiy

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, jsonify
+import google.generativeai as genai
+
+key = "" #ENTER YOUR GOOGLE API KEY HERE
+app = Flask(__name__)
+
+genai.configure(api_key=key)
+
+model = genai.GenerativeModel('gemini-pro')
+
+google = model.start_chat(history = [])
+
+@app.route("/", methods = ["GET","POST"])
+def main():
+    global google
+    google = model.start_chat(history = [])
+    return render_template("index.html")
+
+
+@app.route("/chat",methods = ["POST"])
+def chat():
+    
+    data = request.json
+    reply = google.send_message(data['prompt'])
+    ret = {}
+    ret['prompt'] = data['prompt']
+    ret['resp'] = reply.text
+    return jsonify(ret)
+
+if __name__ == "__main__" :
+    app.run(debug=False)
